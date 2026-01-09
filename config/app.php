02602899b1081772d3d38d79f1cbba6fe4b5c3b3
Shardium <nitroshard@gmail.com>
2026-01-09 22:54:42 +0700

    Update stuff ig

--- config/app.php
+++ config/app.php
@@ -52,8 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
-
+    'url' => env('APP_URL', 'http://water-quest.vercel.app'),
+    
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
